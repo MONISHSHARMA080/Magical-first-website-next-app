05a68520b585209616d36e5cac6384b277f4970d
Monish <monishsharma080@gmail.com>
2024-05-04 17:15:26 +0530

    working without volume

--- Dockerfile
+++ Dockerfile
@@ -1,52 +1,5 @@
-# # Build the Go binary
-# FROM golang:1.22 AS go-build
-# WORKDIR /app
-# COPY . /app/.
-# RUN go build a.go
-
-# # Build the Next.js app
-# FROM node:18 AS node-build
-# WORKDIR /app
-# COPY package.json package-lock.json ./
-# RUN npm install
-# RUN npm ci
-# COPY . .
-# RUN npm run dev
-
-# # Final stage
-# FROM node:18
-# WORKDIR /app
-# COPY --from=go-build /app/main ./
-# COPY --from=node-build /app/.next ./.next
-# COPY --from=node-build /app/node_modules ./node_modules
-# COPY --from=node-build /app/package.json ./package.json
-
-# EXPOSE 3000
-# EXPOSE 3333
-
-# VOLUME /app
-
-# CMD ["npm", "run", "dev"]
-
-# FROM golang:1.22 AS go-build
-
-# # Set the working directory inside the container
-# WORKDIR /app
-
-# # Copy the Go source code
-# COPY a.go .
-
-# # Build the Go binary
-# RUN go build -o a a.go
-# CMD ./a 
-
-# Stage 1: Build the Go binary
-
-# Command to run the executable
-# CMD ["./goserver"]
-
 # Stage 2: Build the Next.js app
-FROM node:latest AS node-build
+FROM node:21-alpine3.18 AS node-build
 WORKDIR /app
 COPY package.json package-lock.json ./
 RUN npm install
@@ -58,11 +11,6 @@ WORKDIR /app
 
 # Copy built Next.js files
 COPY --from=node-build /app/. .
-# COPY --from=node-build /app/public ./public
-
-# Copy built Go binary
-# COPY --from=golangBuild /app/goserver goserver
-
 # Expose ports for Go and Next.js
 EXPOSE 3000
 # EXPOSE 3333
@@ -70,8 +18,6 @@ EXPOSE 3000
 # Command to run both Go and Next.js
 CMD npm run dev
 
-
-# CMD /app/./a & npm run build
 
 # docker build -t nextjs-docker . ; docker run -p 3000:3000 -p 3333:3333 -v $(pwd):/app nextjs-docker
 
--- a.go
+++ a.go
@@ -10,7 +10,7 @@ import (
 
 func getRoot(w http.ResponseWriter, r *http.Request) {
 	fmt.Printf("\n---got the request---\n")
-	fmt.Printf("\n------\n")
+	fmt.Printf("\n----in getroot--\n")
 	io.WriteString(w, "This is my website!\n")
 }
 func getHello(w http.ResponseWriter, r *http.Request) {
@@ -19,10 +19,39 @@ func getHello(w http.ResponseWriter, r *http.Request) {
 	io.WriteString(w, "Hello, HTTP!\n")
 }
 
+func file(w http.ResponseWriter, r *http.Request) {
+	fmt.Printf(" \n -->> files func in  in the Next.js directory:\n")
+
+	// Open the directory where Next.js files are mounted
+	dir, err := os.Open("/app")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		println("\n got the erron -->",err)
+		return
+	}
+	defer dir.Close()
+	
+	// Read the directory contents
+	files, err := dir.Readdir(-1)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		println("\n got the error -->",err)
+		return
+	}
+
+	// Write file names to the response
+	for _, file := range files {
+		io.WriteString(w, file.Name()+"\n")
+	}
+}
+
 func main() {
 	http.HandleFunc("/", getRoot)
 	http.HandleFunc("/hello", getHello)
+	http.HandleFunc("/file", file)
+
 	fmt.Printf("\n\n  ----------- go version running  -------------\n\n")
+	fmt.Printf("\n\n  ----------- go server listening on port http://localhost:4696   -------------\n\n")
 	err := http.ListenAndServe(":4696", nil)
 	if errors.Is(err, http.ErrServerClosed) {
 		fmt.Printf("server closed\n")
@@ -31,16 +60,5 @@ func main() {
 		os.Exit(1)
 
 	}
-	// for {
-	// 	http.HandleFunc("/", getRoot)
-	// http.HandleFunc("/hello", getHello)
-	// fmt.Printf("\n\n  ----------- go version running  -------------\n\n")
-	// err := http.ListenAndServe(":4696", nil)
-	// if errors.Is(err, http.ErrServerClosed) {
-	// 	fmt.Printf("server closed\n")
-	// } else if err != nil {
-	// 	fmt.Printf("error starting server: %s\n", err)
-	// 	os.Exit(1)
-	// }
-	// }
+	
 }
